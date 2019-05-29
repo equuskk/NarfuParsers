55c8b74e46990017ba76371e944485256f69fe97
equus <kirill.kuzn98@gmail.com>
2019-05-29 12:31:53 +0300

    Исправить пример использования библиотеки

--- example/ExampleApp/Program.cs
+++ example/ExampleApp/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using NarfuParsers.Parsers;
 using NarfuParsers.Schedule;
-using NarfuParsers.Common;
 
 namespace ExampleApp
 {
@@ -12,28 +9,21 @@ namespace ExampleApp
     {
         static async Task Main(string[] args)
         {
-            var teacherSchedule = new TeachersSchedule();
+            var timeout = TimeSpan.FromSeconds(5);
+
+            var teacherSchedule = new TeachersSchedule(timeout);
             var teacherLessons = await teacherSchedule.GetLessons(22914);
 
-            var studentsSchedule = new StudentsSchedule();
+            var studentsSchedule = new StudentsSchedule(timeout);
             var studentsLessons = await studentsSchedule.GetLessons(9092);
 
-            var schoolsParser = new SchoolsParser();
+            var schoolsParser = new SchoolsParser(timeout);
             var schools = await schoolsParser.GetSchools();
 
-            var groupsParser = new GroupsParser();
+            var groupsParser = new GroupsParser(timeout);
             var groups = await groupsParser.GetGroupsFromSchool(15);
 
-            var httpHandler = new HttpClientHandler
-            {
-                Proxy = new WebProxy(new Uri("http://***.***.***.***:****"))
-            };
-            var httpClient = new HttpClient(httpHandler)
-            {
-                BaseAddress = new Uri(Constants.EndPoint)
-            };
-
-            var teachersParser = new TeachersParser(httpClient);
+            var teachersParser = new TeachersParser(timeout);
             var teachers = await teachersParser.GetTeachersInRange(22913, 22917, 3, 1000);
 
             Console.ReadLine();
