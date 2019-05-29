71a7e9bf40c0c0a9370cb8c89b4b7c8b3347c691
equus <kirill.kuzn98@gmail.com>
2019-05-29 12:32:05 +0300

    Добавить пример использования библиотеки с прокси

---
+++ example/ExampleAppWithProxy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Flurl.Http;
+using NarfuParsers;
+
+namespace ExampleAppWithProxy
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            ConfigureProxy("https://*.*.*.*:3128");
+            var timeout = TimeSpan.FromSeconds(5);
+
+            var scheduleService = new ScheduleService(timeout);
+            var lessons = await scheduleService.Students.GetLessons(9092);
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void ConfigureProxy(string url)
+        {
+            FlurlHttp.Configure(settings => {
+                settings.HttpClientFactory = new ProxyHttpClientFactory(url);
+            });
+        }
+    }
+}
---
+++ example/ExampleAppWithProxy/ProxyHttpClientFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Http;
+using Flurl.Http.Configuration;
+
+namespace ExampleAppWithProxy
+{
+    public class ProxyHttpClientFactory : DefaultHttpClientFactory
+    {
+        private readonly string _address;
+
+        public ProxyHttpClientFactory(string address)
+        {
+            _address = address;
+        }
+
+        public override HttpMessageHandler CreateMessageHandler()
+        {
+            return new HttpClientHandler
+            {
+                Proxy = new WebProxy(_address),
+                UseProxy = true
+            };
+        }
+    }
+}
